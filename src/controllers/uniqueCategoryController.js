bbc45f27ad9b9e7b5a8a240e0eb2cfc61b8dbae9
Rakibul Hasan <rakibulhasan745101@gmail.com>
2025-04-02 02:09:21 +0600

    unique-category-controller is done

--- src/controllers/uniqueCategoryController.js
+++ src/controllers/uniqueCategoryController.js
@@ -3,6 +3,17 @@ import { client } from "../db/mongo.config.js";
 export const uniqueCategoryController = async (req, res) => {
   const categories = await client
     .db("wordify-blog")
-    .runCursorCommand({ distinct: "blogs", key: "category" });
-  res.send(categories);
+    .collection("blogs")
+    .find({})
+    .project({ category: 1, _id: 0 })
+    .toArray();
+
+  const uniqueCategories = [];
+
+  categories.forEach((categoryObj) => {
+    !uniqueCategories.includes(categoryObj.category) &&
+      uniqueCategories.push(categoryObj.category);
+  });
+
+  res.send(uniqueCategories);
 };
