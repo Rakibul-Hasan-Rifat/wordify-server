030c023acbb2d10294db223c683c46979bba0e9f
Rakibul Hasan <rakibulhasan745101@gmail.com>
2025-04-02 16:44:25 +0600

    The functionality of search by text in the title and fliter by category is added

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,13 +1,17 @@
 import { client } from "../db/mongo.config.js";
 
 export const searchController = async (req, res) => {
-  const searchQuery = req.query?.text || '';
-  
+  const searchQuery = req.query?.title || "";
+  const categoryQuery = req.query?.category || "";
+
   const searchResult = await client
     .db("wordify-blog")
     .collection("blogs")
-    .find({ title: { $regex: searchQuery, $options: "i" } })
+    .find({
+      title: { $regex: searchQuery, $options: "i" },
+      category: categoryQuery,
+    })
     .toArray();
 
-    res.send(searchResult);
+  res.send(searchResult);
 };
